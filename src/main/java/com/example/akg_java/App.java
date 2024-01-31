bc79d385b17106daf476ed46601ad1ce2850991d
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2024-01-31 23:36:27 +0300

    rastorization fix

--- src/main/java/com/example/akg_java/App.java
+++ src/main/java/com/example/akg_java/App.java
@@ -13,13 +13,13 @@ public class App extends JComponent implements ActionListener, KeyListener {
     private static final int HEIGHT = 800;
     private static final int HEADER = 40;
     private static final int WIDTH = 1600;
-    private static final String fileName = "D:/LABS/AKG/AKG_LAB1_OBJ_PARSER/dress.obj";
+    private static final String fileName = "D:/LABS/AKG/AKG_LAB1_OBJ_PARSER/porshe.obj";
     private static JFrame frame;
     private Robot inputs;
     private long prev;
     private Graphics graphics;
     private Mesh input;
-    private Timer timer = new Timer(5, this);
+    private Timer timer = new Timer(1, this);
     private double angle = 0;
     public static void main(String[] args) throws AWTException, IOException {
         BufferedImage buffer = new BufferedImage(WIDTH + 1, HEIGHT + 1, BufferedImage.TYPE_INT_RGB);
@@ -42,14 +42,15 @@ public class App extends JComponent implements ActionListener, KeyListener {
         timer.start();
     }
 
-    public Matr4x4 matr = Matr4x4.getCameraMatrix(Matr4x4.identity())
+    public Matr4x4 matr = Matr4x4.translation(0, 0, 100)
+            .multiply(Matr4x4.getCameraMatrix(Matr4x4.identity()))
             .multiply(Matr4x4.projection(90, (double) HEIGHT / WIDTH, 0.1, 1000.0f))
             .multiply(Matr4x4.screen(WIDTH, HEIGHT));
 
     @Override
     public void paint(java.awt.Graphics g) {
         if (this.input != null) {
-            angle += (System.currentTimeMillis() - prev) / 1000.0 * 60;
+/*            angle += (System.currentTimeMillis() - prev) / 1000.0 * 90;*/
             frame.setTitle(String.format("%d fps", (int) (1000 / (System.currentTimeMillis() - prev))));
             prev = System.currentTimeMillis();
             if (angle > 360) {
@@ -57,14 +58,15 @@ public class App extends JComponent implements ActionListener, KeyListener {
             }
             graphics.clear(Color.BLACK.getIntArgbPre());
             Matr4x4 model = Matr4x4.rotationY(angle)
-                    .multiply(Matr4x4.translation(0, 0, 5))
                     .multiply(matr);
             for (Triangle triangle: input.getTris()) {
                 Vec3d[] v = triangle.getPoints();
 /*                graphics.drawTriangle(new Triangle(v[0].multiply(model), v[1].multiply(model), v[2].multiply(model))
                         , Color.WHITE.getIntArgbPre());*/
+/*                graphics.rasterBarycentric(new Triangle(v[0].multiply(model), v[1].multiply(model), v[2].multiply(model)),
+                        WIDTH, HEIGHT, Color.WHITE.getIntArgbPre());*/
                 graphics.test(new Triangle(v[0].multiply(model), v[1].multiply(model), v[2].multiply(model)),
-                        Color.WHITE.getIntArgbPre());
+                       Color.GREEN.getIntArgbPre());
             }
             g.drawImage(graphics.getBuffer(), 0, 0, null);
         }
--- src/main/java/com/example/akg_java/math/Graphics.java
+++ src/main/java/com/example/akg_java/math/Graphics.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 
 public class Graphics {
     private BufferedImage buffer;
-    private int width;
-    private int height;
+    private final int width;
+    private final int height;
 
     public Graphics(BufferedImage buffer, int width, int height) {
         this.buffer = buffer;
@@ -45,18 +45,6 @@ public class Graphics {
         }
     }
 
-    public void fillBottomFlat(Vec3d first, Vec3d second, Vec3d third, int color) {
-        double side1 = (second.x - first.x) / (second.y - first.y);// FILL BOTTOM FLAT TRIANGLE
-        double side2 = (third.x - first.x) / (third.y - first.y);
-        double curx1 = first.x;
-        double curx2 = first.x;
-        for (int scanLine = (int)first.y; scanLine <= second.y; scanLine++) {
-            DDAline((int)curx1, scanLine, (int)curx2, scanLine, color);
-            curx1 += side1;
-            curx2 += side2;
-        }
-    }
-
     public void drawTriangle(Triangle tri, int color) {
         Vec3d[] v = tri.getPoints();
         DDAline((int)v[0].x, (int)v[0].y, (int)v[1].x, (int)v[1].y, color);
@@ -64,23 +52,12 @@ public class Graphics {
         DDAline((int)v[2].x, (int)v[2].y, (int)v[0].x, (int)v[0].y, color);
     }
 
-    public void fillTopFlat(Vec3d first, Vec3d second, Vec3d third, int color) {
-        double side1 = (third.x - first.x) / (third.y - first.y);// FILL TOP FLAT TRIANGLE
-        double side2 = (third.x - second.x) / (third.y - second.y);
-        double curx1 = third.x;
-        double curx2 = third.x;
-        for (int scanLine = (int)third.y; scanLine > third.y; scanLine--) {
-            DDAline((int)curx1, scanLine, (int)curx2, scanLine, color);
-            curx1 -= side1;
-            curx2 -= side2;
-        }
-    }
-
     public void sortVerticesByY(Triangle input) {
         Comparator<Vec3d> comparator = Comparator.comparingDouble(vert -> vert.y);
         Arrays.sort(input.getPoints(), comparator);
     }
 
+    // sweep line algorithm
     public void test(Triangle input, int color) {
         sortVerticesByY(input);
         Vec3d[] vertices = input.getPoints();
@@ -94,25 +71,43 @@ public class Graphics {
             Vec3d B = second_half ? vertices[1].add((vertices[2].subtract(vertices[1])).grade(beta))
                     : vertices[0].add((vertices[1].subtract(vertices[0])).grade(beta));
             if (A.x>B.x) Vec3d.swap(A, B);
-/*            DDAline((int)Math.round(A.x), (int)Math.round(A.y), (int)Math.round(B.x), (int)Math.round(B.y), color);*/
             for (int j=(int)Math.round(A.x); j<=B.x; j++) {
-                buffer.setRGB(j, (int)Math.round(vertices[0].y+i), color); // attention, due to int casts t0.y+i != A.y
+                buffer.setRGB(j, (int)Math.round(vertices[0].y+i), color);
             }
         }
     }
 
-    public void rasterize(Triangle input, int color) {
-        sortVerticesByY(input);
-        Vec3d[] vertices = input.getPoints();
-        if (vertices[1].y == vertices[2].y) {
-            fillBottomFlat(vertices[0], vertices[1], vertices[2], color);
-        } else if (vertices[0].y == vertices[1].y) {
-            fillTopFlat(vertices[0], vertices[1], vertices[2], color);
-        } else {
-            Vec3d divider = new Vec3d((vertices[0].x + vertices[2].x) / 2,
-                    vertices[1].y, 0);
-            fillBottomFlat(vertices[0], vertices[1], divider, color);
-            fillTopFlat(vertices[1], divider, vertices[2], color);
+    private Vec3d barycentric(Vec3d[] v, Vec3d p) {
+        Vec3d u = new Vec3d(v[2].x- v[0].x, v[1].x - v[0].x, v[0].x - p.x)
+                .Cross(new Vec3d(v[2].y- v[0].y, v[1].y - v[0].y, v[0].y - p.y));
+        if (Math.abs(u.z) < 1) {
+            return new Vec3d(-1, -1, -1);
+        }
+        return new Vec3d(1.f - (u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    }
+
+
+    // more time-consuming
+    public void rasterBarycentric(Triangle input, int width, int height, int color) {//more time-consuming
+        Vec3d bboxmin = new Vec3d(width - 1, height - 1, 0);
+        Vec3d bboxmax = new Vec3d(0, 0, 0);
+        Vec3d clamp = new Vec3d(width - 1, height - 1, 0);
+        Vec3d[] p = input.getPoints();
+        for (int i = 0; i < 3; i++) {
+            bboxmin.x = Math.max(0, Math.min(bboxmin.x, p[i].x));
+            bboxmin.y = Math.max(0, Math.min(bboxmin.y, p[i].y));
+
+            bboxmax.x = Math.min(clamp.x, Math.max(bboxmax.x, p[i].x));
+            bboxmax.y = Math.min(clamp.y, Math.max(bboxmax.y, p[i].y));
+        }
+        Vec3d point = new Vec3d(0, 0, 0);
+        for(point.x = bboxmin.x; point.x <= bboxmax.x; point.x++) {
+            for(point.y = bboxmin.y; point.y <= bboxmax.y; point.y++) {
+                Vec3d bc_vec = barycentric(p, point);
+                if (!(bc_vec.x < 0 || bc_vec.y < 0  || bc_vec.z < 0)) {
+                    buffer.setRGB((int)Math.round(point.x), (int)Math.round(point.y), color);
+                }
+            }
         }
     }
 
